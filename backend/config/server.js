ba95d4090b1caaebe820caf44094799b78b0facc
helloworld <vital.pereg@mail.ru>
2022-01-28 01:45:08 +0300

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a3a25516605c39aa3cb87d36d1e045a7'),
+    },
+  },
+});
